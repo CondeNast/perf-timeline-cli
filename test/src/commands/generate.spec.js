f47e6c64c06be3531c6d3216fd6672db379ebac2
tollmanz <tollmanz@gmail.com>
2018-03-17 09:33:35 -0500

    Add tests for src/commands/generate.js
    This commit adds tests for the first command. It tests the interface to
    ensure that it meets the requirements for inclusion via yargs'
    `commandDir` command discovery method. It intentionally does not test
    anything beyond the interface as functional testing should be done in
    the handler.js file directly.

---
+++ test/src/commands/generate.spec.js
@@ -0,0 +1,43 @@
+const generate = require('../../../src/commands/generate');
+
+describe('src/commands/generate', () => {
+  describe('command', () => {
+    test('should export property', () => {
+      expect(generate.command).toBeTruthy();
+    });
+
+    test('should export a string', () => {
+      expect(typeof generate.command).toBe('string');
+    });
+  });
+
+  describe('desc', () => {
+    test('should export property', () => {
+      expect(generate.desc).toBeTruthy();
+    });
+
+    test('should export a string', () => {
+      expect(typeof generate.desc).toBe('string');
+    });
+  });
+
+  describe('builder', () => {
+    test('should export property', () => {
+      expect(generate.builder).toBeTruthy();
+    });
+
+    test('should export an object', () => {
+      expect(typeof generate.builder).toBe('object');
+    });
+  });
+
+  describe('handler', () => {
+    test('should export property', () => {
+      expect(generate.handler).toBeTruthy();
+    });
+
+    test('should export a function', () => {
+      expect(generate.handler).toBeInstanceOf(Function);
+    });
+  });
+});
