56c37b1a0b647610c54aa584dba04f0a66203bf3
tollmanz <tollmanz@gmail.com>
2018-03-17 09:15:59 -0500

    Add src/utils/client tests
    This commit introduces the first tests to the project. We're using Jest
    for testing and Sinon for stubbing.
    
    The first tests are for the Chrome Headless client send functionality.
    These are quite simple and get us started with the testing set up.

---
+++ test/src/utils/client.spec.js
@@ -0,0 +1,49 @@
+const sinon = require('sinon');
+
+const { send } = require('../../../src/utils/client');
+
+const sandbox = sinon.createSandbox();
+
+describe('client', () => {
+  describe('send', () => {
+    let page;
+
+    beforeEach(() => {
+      page = {
+        _client: {
+          send: sandbox.stub(),
+        },
+      };
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    test('should be a function', () => {
+      expect(send).toBeInstanceOf(Function);
+    });
+
+    test('should call page._client.send', () => {
+      send(page);
+      expect(page._client.send.calledOnce).toBe(true);
+    });
+
+    test('should call page._client.send with correct args', () => {
+      const command = 'foo';
+      const options = { bar: true };
+      send(page, command, options);
+      expect(page._client.send.calledOnceWith(command, options)).toBe(true);
+    });
+
+    test('should propogate error when page._client.send throws', async () => {
+      page._client.send.throws(new Error());
+
+      try {
+        await send(page);
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+      }
+    });
+  });
+});
