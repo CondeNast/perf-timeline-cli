2df1c2f5e03926c941eba65452bfe5a0db199600
tollmanz <tollmanz@gmail.com>
2018-04-08 21:01:50 -0500

    WIP

--- test/integration/commands/generate.spec.js
+++ test/integration/commands/generate.spec.js
@@ -1,5 +1,7 @@
 const yargs = require('yargs');
 
+const { bool } = require('../../helper/args');
+
 const { builder, command, desc } = require('../../../src/commands/generate');
 const { handler } = require('../../helper/test-handler');
 
@@ -15,19 +17,6 @@ const cli = yargs.command(testCommand);
 
 describe.only('src/commands/generate', () => {
   describe('ignore-https-errors', () => {
-    test('should return true', () => {
-      const output = cli.parse('generate url --ignore-https-errors');
-      expect(output.ignoreHttpsErrors).toBe(true);
-    });
-
-    test('should be false by default', () => {
-      const output = cli.parse('generate url');
-      expect(output.ignoreHttpsErrors).toBe(false);
-    });
-
-    test('should be false when the no variant is passed', () => {
-      const output = cli.parse('generate url --no-ignore-https-errors');
-      expect(output.ignoreHttpsErrors).toBe(false);
-    });
+    bool(cli, 'ignore-https-errors');
   });
 });
